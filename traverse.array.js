933cee777cfebeef4150ed76b3910a38d5740bd8
Chimsom <12412676-The-Chimsom@users.noreply.replit.com>
2024-05-18 06:00:46 +0000

    create a function that traverses an array

---
+++ traverse.array.js
@@ -0,0 +1,9 @@
+// This function implements traversing an array.
+
+function traverse(arr){
+  for(let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+  }
+}
+
+traverse(['me', 'you', 'them'])
